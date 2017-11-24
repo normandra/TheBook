27238391a02e7f6ae75214acfde45e911f217487
normandra <normandra@users.noreply.github.com>
2017-11-24 23:23:54 +0100

    latest update2

---
+++ Assets/Scripts/ForrestOptions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ForrestOptions : MonoBehaviour {
+
+	public ActManager am;
+	public Actors[] actors;
+
+	public void changeToRun(){
+		foreach (var actors in actors) {
+			actors.actor.GetComponent<Actor> ().setAnimation (actors.param);
+		}
+			
+		this.Invoke ("changeToRunForrest", 1f);
+
+	}
+
+	public void changeToRunForrest(){
+		am.sc.FadeAndLoadScene ("5SwitzerlandRun");
+	}
+
+	public void stay(){
+		am.DisplayNextAct ();
+	}
+
+}
